791fb4f8e5a453fddd0f822fa712cdbde148dea1
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-11-06 16:23:15 -0300

    savings.h

---
+++ src/savings.h
@@ -0,0 +1,14 @@
+#ifndef SRC_SAVINGS_H
+#define SRC_SAVINGS_H
+
+#include "lib/data.h"
+
+class Savings {
+
+public:
+
+    PathList operator()(const Instance&) const;
+
+};
+
+#endif // SRC_SAVINGS_H
