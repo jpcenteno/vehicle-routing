727a7b6e6d8f35173d0b631a3e967d4413d1fb4e
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-11-06 16:24:33 -0300

    main

--- src/vrp.cpp
+++ src/vrp.cpp
@@ -1,8 +1,19 @@
 #include <iostream>
 
+#include "lib/CLI11.hpp"
 #include "lib/data.h"
 
-int main(void) {
+
+int main(int argc, char * argv[]) {
+
+    // CLI
+    CLI::App app {"TP3 - Aed3"};
+
+    std::string algorithm;
+    app.add_option("-a,--algo", algorithm,
+            "algoritmo. Puede ser `savings`");
+
+    CLI11_PARSE(app, argc, argv);
 
     // Lee STDIN
     const Instance instance;
