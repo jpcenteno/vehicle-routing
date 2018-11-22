6ddcfb2c37cd57637e823c1ed3e3b49ba2abeefa
cliberman <cliberman@dc.uba.ar>
2018-11-22 14:19:25 -0300

    algunos cambios en goloso.cpp

--- src/goloso.cpp
+++ src/goloso.cpp
@@ -12,27 +12,26 @@ PathList Goloso::operator()(const Instance& instance) const {
 	unsigned int carga;
 	size_t indice_min, i;
     float min;
-	unsigned int C = instance.getCapacity();
+	unsigned int C = static_cast<unsigned int> (instance.getCapacity());
 
 
-	while(cant_nodos < n){
-		vector<size_t> camino;
-		carga = 0;
-		indice_min = 0;
-		min = 0;
-		while(((carga + nodos[indice_min].demand) <= static_cast<unsigned int>(C)) && (cant_nodos < n)){ 
-			carga += nodos[indice_min].demand;
-			camino.push_back(indice_min);
-			visitados[indice_min] = true;
-			costo += min; 
-		
-			if(indice_min != 0) cant_nodos++;   //Para no sumar el cero cada vez
-			min = INT_MAX;
-			i = indice_min;
+    while(cant_nodos < n){
+        vector<size_t> camino;
+        carga = 0;
+        indice_min = 0;
+        min = 0;
+        while(((carga + nodos[indice_min].demand) <= C) && (cant_nodos < n)){
+            carga += nodos[indice_min].demand;
+            camino.push_back(indice_min);
+            visitados[indice_min] = true;
+            costo += min;
 
-			for(size_t j=0; j<n; j++){
-				/*si era infactible lo pongo como visitado para no volverlo a visitar otra vez*/
-                if(nodos[j].demand > C && !visitados[j]){		
+            if(indice_min != 0) cant_nodos++;   //Para no sumar el cero cada vez
+            min = INT_MAX;
+            i = indice_min;
+
+            for(size_t j=0; j<n; j++){
+                if(nodos[j].demand > C && !visitados[j]){		//si era infactible lo pongo como vistado para no volverlo a visitar otra vez.
                     visitados[j] = true;
                     cant_nodos++;
                 }
@@ -41,28 +40,18 @@ PathList Goloso::operator()(const Instance& instance) const {
                     indice_min = j;
                 }
             }
-		} 
-	
-		costo += dist[camino.back()][0];
-		camino.push_back(0);
-		if(camino.size() > 2){		//Evito agregar caminos de tipo (0,0) cuando en el medio hubo infactibles por ejemplo.
-			rutas.first.push_back(camino);
-		}	
-	}
-		
-		rutas.second = costo;
-		return rutas;
-		
-		/*Salida de Prueba*/
-		/*cout << rutas.size() << endl;
-		for(int i= 0; i<rutas.size();i++){
-			for(int j= 0; j<rutas[i].size();j++){
-			cout << rutas[i][j] << " " ;
+        }
 
-			}
-			cout << endl;
-		}
-		cout << costo <<endl;*/
+        costo += dist[camino.back()][0];
+        camino.push_back(0);
+        if(camino.size() > 2){		//Evito agregar caminos de tipo (0,0) cuando en el medio hubo infactibles por ejemplo.
+            rutas.first.push_back(camino);
+        }
+    }
+
+    rutas.second = costo;
+    return rutas;
+
 
 }
 
