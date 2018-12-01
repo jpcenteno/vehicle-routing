ebda2c4b55f7fd8956a88b067756e372d3e21637
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-12-01 14:35:11 -0300

    externalizar defs del algoritmo SA

---
+++ src/aux/sa_defs.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstddef>
+#include <cstdint>
+
+using PathId      = size_t;
+using NodeId      = size_t;
+using PathIndex   = size_t;
+using Length      = uint32_t;
+using LengthDelta = int32_t;
+using Quantity    = uint32_t;
