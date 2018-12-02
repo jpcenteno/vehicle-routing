d7a6fa72e50cde430ce841369376ef5361009b3d
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2018-12-01 23:43:26 -0300

    implementación de SA pasada en limpio

--- src/simulated_annealing.cpp
+++ src/simulated_annealing.cpp
@@ -3,21 +3,56 @@
 #include <numeric>
 #include <limits>
 
-
 #include "simulated_annealing.h"
+#include "aux/sa_defs.h"
+#include "aux/sa_path.h"
+#include "aux/sa_solution.h"
 #include "savings.h"
 
-/** Tira una "moneda" aleatoria con una probabilidad dada */
-inline bool rand(float probability) {
-
-    // genera r ~ U(0, 1)
+/** revuelve un float en ~U(0,1) */
+inline float rand_uniform() {
     float r = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-    return r < probability;
-
+    return r;
 }
 
 
-PathList SimulatedAnneling::operator()(Instance&,
-                        uint32_t n_resets = 3,
-                        float t           = 10,
-                        float beta        = static_cast<float>(0.7));
+PathList SimulatedAnneling::operator()(const Instance& in) const {
+
+    const PathList pl = Savings()(in);
+
+    SASolution s(in, pl);
+    SASolution s_best(s); // Mejor solución conocida hasta el momento
+
+    std::vector<SASolution::NodeExchange> neighbors;
+    s.getNeighbors(neighbors);
+
+    auto it_max_delta = std::max_element(neighbors.begin(), neighbors.end(),
+        [&s](const auto& exc_a, const auto& exc_b){
+            return s.delta(exc_a) < s.delta(exc_b);
+        });
+    float t = static_cast<float>(s.delta(*it_max_delta));
+
+    uint32_t k = 0;
+    while ( k  < args::max_iters ) {
+
+        s.getNeighbors(neighbors);
+        for ( const SASolution::NodeExchange& exc : neighbors ) {
+
+            LengthDelta delta = s.delta(exc);
+
+            if (delta < 0 or (- delta / t) < rand_uniform()) {
+
+                s.acceptExchange(exc);
+                if ( s.get_length() < s_best.get_length() ) {
+                    s_best = s;
+                }
+                break;
+
+            }
+        }
+
+        t = t * args::beta;
+    }
+
+    return s_best.to_pathlist();
+}
--- src/simulated_annealing.h
+++ src/simulated_annealing.h
@@ -3,17 +3,10 @@
 #include <cstdint>
 
 #include "lib/data.h"
+#include "lib/args.hpp"
 #include "aux/sa_defs.h"
 
 class SimulatedAnneling {
-
 public:
-
-    PathList operator()(Instance&,
-                        uint32_t n_resets = 3,
-                        float t           = 10,
-                        float beta        = static_cast<float>(0.7));
-
-private:
-
+    PathList operator()(const Instance&) const;
 };
