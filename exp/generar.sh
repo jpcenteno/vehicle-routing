afce15db703dfc5ddd0e8b0750234a6ddfccd5c4
Tomas Caballero <tomycaballero95@gmail.com>
2018-12-09 19:44:16 -0300

    Generación de instancias de experimentación para medición de tiempos

--- exp/generar.sh
+++ exp/generar.sh
@@ -1,8 +1,6 @@
 #! /bin/bash
 
-mkdir -p EXP
-
-# Genera instancias de vrp
+# Genera instancias de vrp en la carpeta instances
 for (( i = 10; i <= 300; i++ )); do
-	echo $i | ./gen > EXP/"instance_$i"
+	echo $i | ./gen > instances/"$i"
 done 
--- exp/run.sh
+++ exp/run.sh
@@ -1,16 +1,15 @@
 #! /bin/bash
 
-# Lee las instancias de una carpeta y las ejecuta
+# Lee las instancias de la carpeta instances de forma ordenada y las ejecuta para un algoritmo en particular
 
 declare -a files
 
 i=1
-for vrp in EXP/*
+for vrp in `ls instances/ | sort -n`
 do
 	files[i++]="${vrp}"
 done
 
 for (( i = 1; i <= ${#files[@]}; i++ )); do
-	#echo "${files[i]}"
-	./vrp -a goloso < "${files[i]}"
+	./vrp -a 2-opt -e < "instances/${files[i]}"
 done 
